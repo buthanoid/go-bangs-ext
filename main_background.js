6ac2fe827afe9f5bd08e7013ed60fa20d944bcbb
buthanoid <67276561+buthanoid@users.noreply.github.com>
2020-06-23 16:33:41 +0200

    version 0.2
    Selecting the default suggestion will try to load the first suggestion

--- main_background.js
+++ main_background.js
@@ -33,6 +33,7 @@ function init () {
 
 var bangs ; // contains the data. shared between the handlers.
 var loaded = false ; // says if bangs have been loaded. shared between the handlers.
+var user_input ; // see comments for the function handler_input_enter
 
 // called when the user type the extension keyword and a space in the omnibox
 function handler_input_start () {
@@ -46,6 +47,7 @@ function handler_input_start () {
 	// time we change the options and want to see the changes.
 
 	loaded = false ; // used to prevent "handler_input_change" to read variable "bangs" before it is written
+	user_input = "" ;
 	storage_get_key("bangs")
 	.then(res_bangs => {
 		bangs = res_bangs ;
@@ -63,45 +65,79 @@ function handler_input_change (input_str, make_suggests) {
 		return 
 	}
 	
-	// Separate the keyword (bang) from the params (optional additional words)
-	var input_keyword, input_param ;
-	var index = input_str.indexOf(" ");
-	if (index == -1) {
-		input_keyword = input_str ;
-		input_param = "" ;
-	}
-	else {
-		input_keyword = input_str.substring(0, index) ;
-		input_param = input_str.substring(index + 1);
-	}
-	
-	// We get the bangs that start with the input key 
-	var filtered_bangs = bangs.filter (bang => {
-		return bang[BANG].startsWith(input_keyword) ;
-	}) ;
+	user_input = input_str ;
+	var input = parse_input(user_input) ;
+	var filtered_bangs = find_bangs(input.key, bangs);
 	
 	// we build the suggestions 
 	var suggestions = filtered_bangs.map (bang => {
-		var url_str ;
-		if (input_param == "" || bang[URL_BEFORE_PARAM] == "") 
-			url_str = bang[URL_WITHOUT_PARAM] ;
-		else url_str = bang[URL_BEFORE_PARAM] + input_param + bang[URL_AFTER_PARAM] ;
-		var description = "(" + bang[BANG] + ") " + bang[LABEL] + " " + input_param ;
-		return { content: url_str, description: description };
+		return build_suggestion (bang, input.param) ;
 	}) ;
 
 	make_suggests(suggestions); // we send them to the omnibox
 }	
 
 // Called every time the user selects a suggestion.
-// Note that the first suggestion is a special one that do not trigger this handler.
-// It is imposed by the API I would gladly remove this special one.
-// I took this function from Mozilla Tutorial,
-// its point is just to create the tab in the way the user wanted
+// Note that the first suggestion is not a suggestion, only a 
+// description, this is imposed by the Omnibox API.
+// When the first suggestion is selected, the "url" contains the
+// user input, for example "y koala", and this is an incorrect url.
+// We handle this by checking if the url is exactly the user input in the omnibox.
+// If it is the case, we try to load the first suggestion.
+// If the "url" does not match the user input, we assume it is correct and load.
 function handler_input_enter (url, disposition) {
+	
+	// if "handler_input_start" has not written the bangs yet,
+	// we do nothing but will return soon and surely it will be written
+	if (! loaded) {
+		setTimeout (() => { handler_input_enter (url, disposition) }, 100) ;
+		return 
+	}
+	
+	if (url == user_input) { // true when user selects default suggestion,
+		// see comments above
+		var input = parse_input(user_input) ;
+		var filtered_bangs = find_bangs(input.key, bangs);
+		if (filtered_bangs.length < 1) return ;
+		var suggestion = build_suggestion(filtered_bangs[0], input.param);
+		url = suggestion.content ;
+	}
+	
+	// load the url
 	switch (disposition) {
 	case "currentTab" : browser.tabs.update({ url }) ; break ;
 	case "newForegroundTab" : browser.tabs.create({ url }) ; break ;
 	case "newBackgroundTab" : browser.tabs.create({ url, active: false }) ; break ;
 	}
+}
+
+// Separate the keyword (bang) from the params (optional additional words)
+// string -> object{key:string, param:string}
+function parse_input (str) {
+	var index = str.indexOf(" ");
+	if (index == -1) return { key: str, param: "" } ;
+	else return {
+		key: str.substring(0, index),
+		param: str.substring(index + 1)
+	};
+}
+
+// string, array[array[5 string]] -> array[array[5 string]]
+// from a string find the corresponding bangs
+function find_bangs (str_key, bangs) {
+	// We get the bangs that start with the bang key 
+	var filtered_bangs = bangs.filter (bang => {
+		return bang[BANG].startsWith(str_key) ;
+	}) ;
+	return filtered_bangs ;
+}
+
+// build a suggestion object from a bang and an optionnal input param
+// array[5 string], string -> object{content:string, description:string}
+function build_suggestion (bang, param) {
+	var description = "(" + bang[BANG] + ") " + bang[LABEL] + " " + param ;
+	var content = "" ;
+	if (param == "" || bang[URL_BEFORE_PARAM] == "") content = bang[URL_WITHOUT_PARAM] ;
+	else content = bang[URL_BEFORE_PARAM] + param + bang[URL_AFTER_PARAM] ;
+	return { content: content, description: description };
 }
